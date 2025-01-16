4a21649efeebf69a2012800822618ffcc472b017
deepend15 <170750487+deepend15@users.noreply.github.com>
2025-01-16 15:07:56 -0500

    Add all code to play game through the console
    -accounts for game-over scenarios - draws, & winner via every possible
    3-in-a-row combination
    -prevents attempting to play another round once game is over
    -prevents invalid row/column selections
    -some of above will be modified/removed once game is moved from the
    console to the UI

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function gameboard() {
+const gameboard = (function () {
     const rows = 3;
     const columns = 3;
     const board = [];
@@ -13,7 +13,12 @@ function gameboard() {
     const getBoard = () => board;
 
     const selectSquare = (row, column, player) => {
-        if (board[row][column] === null) {
+        if (row > board.length - 1 ||
+            column > board[0].length - 1 ||
+            (row || column) < 0) {
+            console.log(`Invalid selection. Try again.`);
+            return false;
+        } else if (board[row][column] === null) {
             board[row][column] = player.token;
         } else {
             console.log(`You can't select that square! Try again.`);
@@ -24,27 +29,17 @@ function gameboard() {
     const printBoard = () => console.log(board);
 
     return { getBoard, selectSquare, printBoard };
-}
+})();
 
-function createPlayerOne(name) {
-    const token = 'x';
-
+function createPlayer(name, token) {
     return { name, token };
 }
 
-function createPlayerTwo(name) {
-    const token = 'o';
-
-    return { name, token };
-}
-
-function gameController(playerOneName = "Jim",
-    playerTwoName = "Bob"
+const game = (function (playerOneName = "Jim",
+                        playerTwoName = "Bob"
 ) {
-    const board = gameboard();
-
-    const playerOne = createPlayerOne(playerOneName);
-    const playerTwo = createPlayerTwo(playerTwoName);
+    const playerOne = createPlayer(playerOneName, 'x');
+    const playerTwo = createPlayer(playerTwoName, 'o');
 
     let activePlayer = playerOne;
 
@@ -57,25 +52,99 @@ function gameController(playerOneName = "Jim",
     };
 
     const printNewRound = () => {
-        board.printBoard();
+        gameboard.printBoard();
         console.log(`${activePlayer.name}'s turn.`);
     };
 
     const playRound = (row, column) => {
-        let squareSelection = board.selectSquare(row, column, activePlayer);
-        if (squareSelection === false) {
-            printNewRound();
+        if (activePlayer === null) {
+            console.log(`Refresh to start a new game!`);
         } else {
-            console.log(`${activePlayer.name} selected row ${row}, column ${column}.`);
-
-            switchPlayerTurn();
+            let squareSelection = gameboard.selectSquare(row, column, activePlayer);
+            if (squareSelection === false) {
             printNewRound();
+            } else {
+                console.log(`${activePlayer.name} selected row ${row}, column ${column}.`);
+
+                const firstRow = gameboard.getBoard()[0];
+                const secondRow = gameboard.getBoard()[1];
+                const thirdRow = gameboard.getBoard()[2];
+                const firstColumn = [firstRow[0], secondRow[0], thirdRow[0]];
+                const secondColumn = [firstRow[1], secondRow[1], thirdRow[1]];
+                const thirdColumn = [firstRow[2], secondRow[2], thirdRow[2]];
+                const downDiag = [firstRow[0], secondRow[1], thirdRow[2]];
+                const upDiag = [firstRow[2], secondRow[1], thirdRow[0]];
+
+                const allLines = [firstRow, secondRow, thirdRow, firstColumn, secondColumn, thirdColumn, downDiag, upDiag];
+
+                function containsNull(arr) {
+                    const nulls = [];
+                    for (const square of arr) {
+                        if (square === null) {
+                            nulls.push('null');
+                        }
+                    };
+                    return nulls[0] !== undefined;
+                }
+
+                function matches(line) {
+                    if (containsNull(line)) {
+                        return false;
+                    } else {
+                        let nonMatchingValue = [];
+                        for (let i = line.length - 1; i > 0; i--) {
+                            if (line[i] !== line[i - 1]) {
+                                nonMatchingValue.push(line[i]);
+                                break;
+                            }
+                        }
+                        if (nonMatchingValue[0] !== undefined) {
+                            return false;
+                        } else {
+                            return true;
+                        };
+                    }; 
+                }
+
+                const checkForWinner = (function () {
+                    let winner = '';
+                    for (line of allLines) {
+                        if (matches(line)) {
+                            winner = 'yes'
+                            break;
+                        } else {
+                            winner = 'no';
+                        }
+                    }
+                    return winner;
+                })();
+            
+                if (checkForWinner === 'yes') {
+                    gameboard.printBoard();
+                    console.log(`GAME OVER. ${activePlayer.name} is the winner!`);
+                    activePlayer = null;
+                } else {
+                    let allSquares = [];
+                    let board = gameboard.getBoard();
+                    for (const row of board) {
+                        for (const square of row) {
+                            allSquares.push(square);
+                        };
+                    }
+                    if (!containsNull(allSquares)) {
+                        gameboard.printBoard();
+                        console.log(`GAME OVER. It's a draw!`);
+                        activePlayer = null;
+                    } else {
+                        switchPlayerTurn();
+                        printNewRound();
+                    }
+                }
+            }
         }
     };
 
     printNewRound();
 
     return { playRound };
-};
-
-const game = gameController();
+})();
